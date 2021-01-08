567d8ab5e803be6d9dca625d7a6e4221bcd3d6cb
joel3216 <joeljaxon3216@gmail.com>
2021-01-08 18:10:35 +0530

    ADD. created a program to read and display a 2D array

--- powersOfTwo.py
+++
@@ -1,4 +0,0 @@
-n=int(input("enter the maximum power of 2 "))
-
-for i in range(n):
-    print("2^"+str(i)+": "+str(2**i))
--- userInputReplaceString.py
+++
@@ -1,7 +0,0 @@
-username=input("enter username with minimum 3 characters")
-
-if len(username)<3:
-    print("username is shorter than 3 characters")
-else:
-    msg="Hello <<UserName>>, How are you?"
-    print(msg.replace("<<UserName>>", username))
