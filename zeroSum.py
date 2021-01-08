c976eafa53df502d19271abb59e061f3abcba50f
joel3216 <joeljaxon3216@gmail.com>
2021-01-08 19:28:43 +0530

    ADD. added try/except block for twoDimensionalArray.py and zeroSum.py

--- zeroSum.py
+++ zeroSum.py
@@ -3,14 +3,18 @@ print("enter the elements")
 
 a=[]
 zeroSum=[]
-for i in range(n):
-    a.append(int(input()))
+try:
+    for i in range(n):
+        a.append(int(input()))
 
-for i in range(n-2):
-    for j in range(i+1,n-1):
-        for k in range(j+1,n):
-            if a[i]+a[j]+a[k]==0:
-                zeroSum.append(str(a[i])+","+str(a[j])+","+str(a[k]))
-                break
+    for i in range(n-2):
+        for j in range(i+1,n-1):
+            for k in range(j+1,n):
+                if a[i]+a[j]+a[k]==0:
+                    zeroSum.append(str(a[i])+","+str(a[j])+","+str(a[k]))
+                    break
+    print(zeroSum)
+
+except:
+    print("An error occured")
         
-print(zeroSum)
