c6983bab80c5c45c6dad9b90efa015a471945136
joel3216 <joeljaxon3216@gmail.com>
2021-01-09 13:14:42 +0530

    REFACTOR. Refactored the code to contain functions and handle exceptions and changed variable names to be more descriptive

--- leapYear.py
+++ leapYear.py
@@ -1,16 +1,21 @@
-year=int(input("enter a four digit year"))
-leap=False
-if year>999:
+def leapYearCheck(year):
+    isLeap=False
     if year%400==0:
-        leap=True
+        isLeap=True
     elif year%100==0:
-        leap=False
+        isLeap=False
     elif year%4==0:
-        leap=True
-
-    if bool(leap):
-        print(str(year)+" is a leap year")
+        isLeap=True
+    return isLeap
+try:
+    year=int(input("enter a four digit year"))
+    if year>999:
+        isLeap=leapYearCheck(year)
+        if bool(isLeap):
+            print(str(year)+" is a leap year")
+        else:
+            print (str(year)+" is not a leap year")
     else:
-        print (str(year)+" is not a leap year")
-else:
-    print(str(year)+" contains less than 4-digits")
+        print(str(year)+" contains less than 4-digits")
+except ValueError:
+    print("invalid year, please enter a 4-digit integer")
