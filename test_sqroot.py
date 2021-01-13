bc81528445e186ab6ee63ea18452f73c602fc626
joel3216 <joeljaxon3216@gmail.com>
2021-01-13 21:44:36 +0530

    REFACTOR. removed user input to simpify testing

--- test_sqroot.py
+++ test_sqroot.py
@@ -3,15 +3,8 @@ import util
 
 class testSqroot(unittest.TestCase):
     def test_sqroot(self):
-        try:
-            number=int(input("enter a positive integer"))
-            if number<=0:
-                raise ValueError
-        except ValueError:
-            print("kindly enter only positive integers")
-        else:
-            sqroot=util.util.sqrt(number)
-            self.assertEqual(sqroot,2,"should be 2 for 4")
+        sqroot=util.util.sqrt(4)
+        self.assertEqual(sqroot,2,"should be 2 for 4")
 
 if __name__ == "__main__":
     unittest.main()
