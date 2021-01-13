4a1b2b9db9fb80997e33acf447ea8d2276e808e6
joel3216 <joeljaxon3216@gmail.com>
2021-01-13 10:32:48 +0530

    ADD. added testcase for testing monthlyPayment function of util class

--- monthlyPayment.py
+++ monthlyPayment.py
@@ -8,14 +8,14 @@ class testMonthlyPayment(unittest.TestCase):
     rate=0
     def test_monthlyPayment(self):
         payment=util.util.monthlyPayment(self.principalLoan,self.years,self.rate)
-        self.assertEqual(payment,5,"should be ")
+        self.assertEqual(payment,377.42,"should be 377.42 for p=20000,r=5%,y=5")
 
 if __name__ == "__main__":
     try:
         if len(sys.argv)>0:
-            testMonthlyPayment.principalLoan=float(sys.argv.pop())
-            testMonthlyPayment.years=float(sys.argv.pop())
             testMonthlyPayment.rate=float(sys.argv.pop())
+            testMonthlyPayment.years=float(sys.argv.pop())
+            testMonthlyPayment.principalLoan=float(sys.argv.pop())
     
     except IndexError:
         print("kindly enter the Principal loan, Years and Rate from the command line")
