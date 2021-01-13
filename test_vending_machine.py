4ed114f48e3d309e751b1633610a71aa9e765ab5
joel3216 <joeljaxon3216@gmail.com>
2021-01-13 09:52:56 +0530

    REFACTOR. separated the testing logic from the coding logic. now contains only test functions

--- test_vending_machine.py
+++ test_vending_machine.py
@@ -1,22 +1,8 @@
 import unittest
+import vendingMachine
 
 class testVendingMachine(unittest.TestCase):
 
-    def getMinNotes(self,change,notes):
-        notesUsed=[]
-        noOfNotes=0
-        for note in reversed(notes):
-            while(change>0):
-                if change-note>=0:
-                    change-=note
-                    noOfNotes+=1
-                    notesUsed.append(note)
-                else:
-                    break
-            if change<=0:
-                break
-        return noOfNotes,notesUsed
-    
     def test_input(self):
         try:
             change=int(input("enter the change to be returned"))
@@ -26,7 +12,7 @@ class testVendingMachine(unittest.TestCase):
             print("please enter a positive integer")
         else:
             notes=[1,2,5,10,50,100,500,1000]
-            noOfNotes,notesUsed=self.getMinNotes(change,notes)
+            noOfNotes,notesUsed=vendingMachine.vendingMachine.getMinNotes(vendingMachine,change,notes)
             #noOfNotesTestCase,notesUsedTestCase,testMessage1,testMessage2=getTestInput(self)
             self.assertEqual(noOfNotes,7,"should be 7 for 237")
             self.assertEqual(notesUsed,[100, 100, 10, 10, 10, 5, 2],"should be this for 237")
