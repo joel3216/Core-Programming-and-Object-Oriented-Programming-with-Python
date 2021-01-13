f6d347bd47959381f33be27eebe277e99bb3be05
joel3216 <joeljaxon3216@gmail.com>
2021-01-13 21:45:24 +0530

    REFACTOR. removed user input to simpify testing

--- test_temperature_conversion.py
+++ test_temperature_conversion.py
@@ -3,17 +3,9 @@ import util
     
 class testTemperatureConversion(unittest.TestCase):    
     def test_temperatureConversion(self):
-        try:
-            unit=input("enter the unit of temperature(celcius or fahrenheit)")
-            if "fahrenheit" not in unit and "celcius" not in unit:
-                raise ValueError
-            temperature=float(input("enter the temperature"))
-        except ValueError:
-            print("kindly enter the proper details")
-        else:
-            convertedTemperature=util.util.temperatureConversion(temperature,unit)
-            print(convertedTemperature)
-            self.assertEqual(convertedTemperature,194,"should be 194f for 90c")
+        convertedTemperature=util.util.temperatureConversion(90,"celcius")
+        print(convertedTemperature)
+        self.assertEqual(convertedTemperature,194,"should be 194f for 90c")
 
 if __name__ == "__main__":
     unittest.main()
