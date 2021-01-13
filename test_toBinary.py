a03ff5db93044581f3dbc33ad88c86e102b32b47
joel3216 <joeljaxon3216@gmail.com>
2021-01-13 21:46:01 +0530

    REFACTOR. removed user input to simpify testing

--- test_toBinary.py
+++ test_toBinary.py
@@ -3,13 +3,8 @@ import util
 
 class testToBinary(unittest.TestCase):
     def test_toBinary(self):
-        try:
-            number=int(input("enter an integer"))
-        except ValueError:
-            print("kindly enter only integers")
-        else:
-            binary=util.util.toBinary(number)
-            self.assertEqual(binary,10001110,"expected 10001110 for 142")
+        binary=util.util.toBinary(142)
+        self.assertEqual(binary,10001110,"expected 10001110 for 142")
 
 if __name__ == "__main__":
     unittest.main()
